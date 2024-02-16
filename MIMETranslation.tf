571f0b5ba2740f62b16ee8091b09c1377b309024
Michael Wu <69779024+WichaelMu@users.noreply.github.com>
2024-02-17 09:42:57 +1100

    Run Terraform FMT

--- MIMETranslation.tf
+++ MIMETranslation.tf
@@ -1,15 +1,15 @@
 locals {
   MIMETypes = {
-    ".html" = "text/html"
-    ".css"  = "text/css"
-    ".js"   = "application/javascript"
-    ".png"  = "image/png"
-    ".jpg"  = "image/jpeg"
-    ".eot"  = "application/vnd.ms-fontobject"
-    ".svg"  = "image/svg+xml"
-    ".ttf"  = "font/ttf"
-    ".woff" = "font/woff"
-    ".woff2"= "font/woff2"
+    ".html"  = "text/html"
+    ".css"   = "text/css"
+    ".js"    = "application/javascript"
+    ".png"   = "image/png"
+    ".jpg"   = "image/jpeg"
+    ".eot"   = "application/vnd.ms-fontobject"
+    ".svg"   = "image/svg+xml"
+    ".ttf"   = "font/ttf"
+    ".woff"  = "font/woff"
+    ".woff2" = "font/woff2"
     ".gif"   = "image/gif"
- }
+  }
 }
--- S3WebsiteObjects.tf
+++ S3WebsiteObjects.tf
@@ -17,15 +17,15 @@
 # }
 
 resource "aws_s3_object" "HarryJoh-ezcv-website-recursive" {
- bucket   = aws_s3_bucket.cloud_resume_site_bucket.id
- for_each = fileset("${var.WebsitePath}", "**/*.*")
+  bucket   = aws_s3_bucket.cloud_resume_site_bucket.id
+  for_each = fileset("${var.WebsitePath}", "**/*.*")
 
- key      = each.value
- source   = "${var.WebsitePath}${each.value}"
+  key    = each.value
+  source = "${var.WebsitePath}${each.value}"
 
- // Default to binary/octet-stream if no match found in map.
- content_type = lookup(local.MIMETypes, regex("\\.[^.]+$", each.value), "binary/octet-stream")
- etag     = filemd5("${var.WebsitePath}${each.value}")
+  // Default to binary/octet-stream if no match found in map.
+  content_type = lookup(local.MIMETypes, regex("\\.[^.]+$", each.value), "binary/octet-stream")
+  etag         = filemd5("${var.WebsitePath}${each.value}")
 }
 
 resource "aws_s3_object" "JavaScript" {
